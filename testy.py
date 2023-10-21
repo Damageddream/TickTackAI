dde8c7405602104a42ca07a0713a246d40923f37
Marcin <marcan.grab@gmail.com>
2023-10-21 16:24:51 +0200

    added functions: terminal, utility, winner, result

---
+++ testy.py
@@ -0,0 +1,20 @@
+# EMPTY = None
+
+# testing1 = [[EMPTY, EMPTY, EMPTY],
+#             [EMPTY, EMPTY, EMPTY],
+#             [EMPTY, EMPTY, EMPTY]]
+# testing2 = [["X", "Y", "X"],
+#             ["X", "Y", "X"],
+#             ["Y", "X" "Y"]]
+
+# for row in testing2:
+#     if EMPTY in row:
+#         print(True)
+#         break
+#     print(False)
+
+def return_one():
+    return 1
+def check_return_one():
+    print(return_one())
+check_return_one()
--- tictactoe.py
+++ tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+import copy
 
 X = "X"
 O = "O"
@@ -23,48 +24,102 @@ def player(board):
     o = 0
     for row in board:
         for mark in row:
-            if mark == "X":
+            if mark == X:
                 x += 1
-            elif mark == "Y":
+            elif mark == O:
                 o += 1
-    if x > o or x == 0:
-        return "X"
+    if o < x:
+        return O
     else:
-        return "Y"
+        return X
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    actions = set()
 
+    for count_row, row in enumerate(board): 
+        for count_mark, mark in enumerate(row): 
+            if mark == EMPTY:
+                actions.add((count_row,count_mark))
+
+    return actions
 
 def result(board, action):
-    """
-    Returns the board that results from making move (i, j) on the board.
-    """
-    raise NotImplementedError
+    
+    player = player(board)
+    if not isinstance(action, tuple) or len(action) != 2:
+        raise NameError('valid action')
+    for mark in action:
+        if not isinstance(mark,int):
+           raise NameError('valid action') 
+    board_copy = copy.deepcopy(board)
+    board_copy[action[0]][action[1]] = player
+    return board_copy
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    def check_rows(board):
+        for row in board:
+            if all(mark == row[0] for mark in row):
+                return row[0]
+        return None
+    def check_columns(board):
+        if(board[0][0] == board[1][0] and board[0][0] == board[2][0]):
+            return board[0][0]
+        if(board[0][1] == board[1][1] and board[0][1] == board[2][1]):
+            return board[0][1]
+        if(board[0][2] == board[1][2] and board[0][2] == board[2][2]):
+            return board[0][2]
+        return None
+    def check_diagonal(board):
+        if(board[0][0] == board[1][1] and board[0][0] ==board[2][2]):
+            return board[0][0]
+        if(board[0][2] == board[1][1] and board[0][2]== board[2][0]):
+            return board[0][2]
+        return None
+    rows = check_rows(board)
+    columns = check_columns(board)
+    diagonal = check_diagonal(board)
 
+    if(rows):
+        return rows
+    elif(columns):
+        return columns
+    elif(diagonal):
+        return diagonal
+    return None
+            
 
 def terminal(board):
-    """
-    Returns True if game is over, False otherwise.
-    """
-    raise NotImplementedError
+    
+    def filled_board(board):
+        for row in board:
+            if EMPTY in row:
+                return False
+        return True
+    check_winner = winner(board)
+    filled = filled_board(board)
+    if(filled or check_winner):
+        return True
+    return False
+    
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    check_winner = winner(board)
+    if(check_winner == X):
+        return 1
+    if(check_winner == O):
+        return -1
+    return 0
 
 
 def minimax(board):
